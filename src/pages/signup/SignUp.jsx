b3a92e2d0f152871a7ad483f291256913bd0bac0
MohamedKhayyal <khayyalmohamed5@gmail.com>
2025-06-23 16:01:36 +0300

    final

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -11,7 +11,6 @@ export default function SignUp() {
   useEffect(() => {
     document.title = "Sign Up";
   }, []);
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
